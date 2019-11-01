1f1baa7d5c9ac225155f6d380dd6cf53b98203b5
THB-account <56601190+THB-account@users.noreply.github.com>
2019-11-01 15:21:42 +0100

    Update sprites.py

--- Python-Code/sprites.py
+++ Python-Code/sprites.py
@@ -10,24 +10,53 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
 
-    def move(self, dx=0, dy=0):
-        if not self.collide_with_walls(dx,dy):
-            self.x += dx
-            self.y += dy
-
-    def collide_with_walls(self, dx=0, dy=0):
-        for wall in self.game.walls:
-            if wall.x == self.x + dx and wall.y == self.y + dy:
-                return True
-        return False
+    def collide_with_walls(self, dir):
+         if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.left - self.rect.width
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+                self.vx = 0
+                self.rect.x = self.x
+         if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vy > 0:
+                    self.y = hits[0].rect.top - self.rect.height
+                if self.vy < 0:
+                    self.y = hits[0].rect.bottom
+                self.vy = 0
+                self.rect.y = self.y
 
     def update(self):
-        self.rect.x = self.x * TILESIZE
-        self.rect.y = self.y * TILESIZE
-
+        self.get_keys()
+        self.x += self.vx * self.game.dt #      dt ist die Zeit im Spiel --> wir machen die Bewegung an der vergangenen Zeit fest(Smooth Movement von Kidscancode Youtube)
+        self.y += self.vy * self.game.dt#       DT IST UNABHÄNGIG VON DER FRAMERATE, wir holen die Zeit z.B. in main.run  für die fps self.dt = self.clock ...
+        self.rect.x = self.x
+        self.collide_with_walls('x')
+        self.rect.y = self.y
+        self.collide_with_walls('y')
+ 
+def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:# für DIAGONALE Bewegungen einfach das elif zu if machen bei allen --> Prozesse können gleichzeitig ablaufen
+            self.vx = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy = PLAYER_SPEED
+        if self.vx != 0 and self.vy != 0:
+            self.vx *= 0.7071 // warum 0.7071
+            self.vy *= 0.7071
+        
 
 class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y):
